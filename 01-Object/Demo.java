4c67a6e5f336b9406cf2ec21a1d0f251763f8a90
Jason Lin <jlin31@stuy.edu>
2021-10-22 18:38:17 -0400

    Double negative test case

--- 01-Object/Demo.java
+++ 01-Object/Demo.java
@@ -7,17 +7,20 @@ public class Demo{
     Point perfSq = new Point(3, -4);
     Point perfSq2 = new Point(-5, 12);
     Point perfSq3 = new Point(Math.sqrt(2), Math.sqrt(2));
+    Point perfSq4 = new Point(-7, -24);
 
     System.out.println("static distance from class Demo");
     System.out.println(distance(P1,P2)); //call1
     System.out.println(distance(origin, perfSq));
     System.out.println(distance(origin, perfSq2));
     System.out.println(distance(origin, perfSq3));
+    System.out.println(distance(origin, perfSq4));
     System.out.println("\nstatic distance from class Point");
     System.out.println(Point.distance(P1,P2)); //call 2
     System.out.println(Point.distance(origin, perfSq));
     System.out.println(Point.distance(origin, perfSq2));
     System.out.println(Point.distance(origin, perfSq3));
+    System.out.println(Point.distance(origin, perfSq4));
     // System.out.println(P1.distanceTo(P2)); //call 3
     //This one is IMPLIED strongly. You cannot tell by the calling style alone,
     //but you can make an educated guess.
