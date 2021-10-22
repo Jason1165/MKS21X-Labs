8cc58653f1fcd6dcb7d16f9445c99a8baceceef5
Jason Lin <jlin31@stuy.edu>
2021-10-22 17:52:15 -0400

    Making sure class usage is proper

--- 01-Object/Demo.java
+++ 01-Object/Demo.java
@@ -5,10 +5,15 @@ public class Demo{
     Point P1 = new Point(1,1);
     Point P2 = new Point(2,2);
 
-    System.out.println(  distance(P1,P2)); //call1
-    System.out.println(  Point.distance(P1,P2)); //call 2
-    System.out.println(  P1.distanceTo(P2)); //call 3
+    System.out.println(distance(P1,P2)); //call1
+    // System.out.println(Point.distance(P1,P2)); //call 2
+    // System.out.println(P1.distanceTo(P2)); //call 3
     //This one is IMPLIED strongly. You cannot tell by the calling style alone,
     //but you can make an educated guess.
+  }
+
+  public static double distance(Point coor1, Point coor2) {
+    double hor = Math.abs(coor1.getX() - coor2.getX());
+    return hor;
   }
  }
