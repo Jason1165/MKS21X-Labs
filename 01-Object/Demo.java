f5250b12afd250fbf4015e892e4f4f96ffe8ebfe
Jason Lin <jlin31@stuy.edu>
2021-10-22 18:36:39 -0400

    Created static distance in Point Class, changed certain test cases to be negative to make sure negatives work

--- 01-Object/Demo.java
+++ 01-Object/Demo.java
@@ -1,19 +1,23 @@
 public class Demo{
-  /* more methods can go here*/
 
   public static void main(String[]args){
     Point P1 = new Point(1,1);
     Point P2 = new Point(2,2);
     Point origin = new Point(0, 0);
-    Point perfSq = new Point(3, 4);
-    Point perfSq2 = new Point(5, 12);
+    Point perfSq = new Point(3, -4);
+    Point perfSq2 = new Point(-5, 12);
     Point perfSq3 = new Point(Math.sqrt(2), Math.sqrt(2));
 
+    System.out.println("static distance from class Demo");
     System.out.println(distance(P1,P2)); //call1
     System.out.println(distance(origin, perfSq));
     System.out.println(distance(origin, perfSq2));
     System.out.println(distance(origin, perfSq3));
-    // System.out.println(Point.distance(P1,P2)); //call 2
+    System.out.println("\nstatic distance from class Point");
+    System.out.println(Point.distance(P1,P2)); //call 2
+    System.out.println(Point.distance(origin, perfSq));
+    System.out.println(Point.distance(origin, perfSq2));
+    System.out.println(Point.distance(origin, perfSq3));
     // System.out.println(P1.distanceTo(P2)); //call 3
     //This one is IMPLIED strongly. You cannot tell by the calling style alone,
     //but you can make an educated guess.
--- 01-Object/Point.java
+++ 01-Object/Point.java
@@ -17,5 +17,11 @@ public class Point{
   public double getY(){
     return y;
   }
-  
+
+  public static double distance(Point coor1, Point coor2) {
+    double hor = Math.abs(coor1.getX() - coor2.getX());
+    double ver = Math.abs(coor1.getX() - coor2.getY());
+    return Math.hypot(hor, ver);
+  }
+
 }
