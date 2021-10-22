31f44b7918cde0ab4d5dfff3ac3c21efe0340f9b
Jason Lin <jlin31@stuy.edu>
2021-10-22 18:03:18 -0400

    Used math.hypot to calculate distance, added 3/4/5 tri test case.

--- 01-Object/Demo.java
+++ 01-Object/Demo.java
@@ -4,8 +4,11 @@ public class Demo{
   public static void main(String[]args){
     Point P1 = new Point(1,1);
     Point P2 = new Point(2,2);
+    Point origin = new Point(0, 0);
+    Point perfSq = new Point(3, 4);
 
     System.out.println(distance(P1,P2)); //call1
+    System.out.println(distance(origin, perfSq));
     // System.out.println(Point.distance(P1,P2)); //call 2
     // System.out.println(P1.distanceTo(P2)); //call 3
     //This one is IMPLIED strongly. You cannot tell by the calling style alone,
@@ -14,6 +17,7 @@ public class Demo{
 
   public static double distance(Point coor1, Point coor2) {
     double hor = Math.abs(coor1.getX() - coor2.getX());
-    return hor;
+    double ver = Math.abs(coor1.getX() - coor2.getY());
+    return Math.hypot(hor, ver);
   }
  }
