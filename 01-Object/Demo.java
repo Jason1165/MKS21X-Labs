d5ab17f5afbfb0dd9afde99f08d0af00770766f7
Jason Lin <jlin31@stuy.edu>
2021-10-22 18:47:14 -0400

    Added one test case in which results were not the same ...

--- 01-Object/Demo.java
+++ 01-Object/Demo.java
@@ -8,6 +8,8 @@ public class Demo{
     Point perfSq2 = new Point(-5, 12);
     Point perfSq3 = new Point(Math.sqrt(2), Math.sqrt(2));
     Point perfSq4 = new Point(-7, -24);
+    Point notO = new Point(1, -1);
+    Point not1 = new Point(-4, 11);
 
     System.out.println("static distance from class Demo");
     System.out.println(distance(P1,P2)); //call1
@@ -15,6 +17,7 @@ public class Demo{
     System.out.println(distance(origin, perfSq2));
     System.out.println(distance(origin, perfSq3));
     System.out.println(distance(origin, perfSq4));
+    System.out.println(distance(notO, not1));
 
     System.out.println("\nstatic distance from class Point");
     System.out.println(Point.distance(P1,P2)); //call 2
@@ -22,8 +25,16 @@ public class Demo{
     System.out.println(Point.distance(origin, perfSq2));
     System.out.println(Point.distance(origin, perfSq3));
     System.out.println(Point.distance(origin, perfSq4));
+    System.out.println(Point.distance(notO, not1));
 
+    System.out.println("\nnon static distanceTo from class Point");
     System.out.println(P1.distanceTo(P2)); //call 3
+    System.out.println(origin.distanceTo(perfSq));
+    System.out.println(origin.distanceTo(perfSq2));
+    System.out.println(origin.distanceTo(perfSq3));
+    System.out.println(origin.distanceTo(perfSq4));
+    System.out.println(notO.distanceTo(not1));
+
     //This one is IMPLIED strongly. You cannot tell by the calling style alone,
     //but you can make an educated guess.
   }
