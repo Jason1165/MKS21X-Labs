c7e5e2e983df9d43f7b5295532a937bb6add54d9
Jason Lin <jlin31@stuy.edu>
2021-10-25 17:28:23 -0400

    getPerimeter, unit circle test cases

--- 02-Triangle/Triangle.java
+++ 02-Triangle/Triangle.java
@@ -1,5 +1,7 @@
 public class Triangle {
+
   private Point v1, v2, v3;
+
 
   public Triangle(Point V1, Point V2, Point V3) {
     v1 = V1;
@@ -7,11 +9,13 @@ public class Triangle {
     v3 = V3;
   }
 
+
   public Triangle(double v1x, double v1y, double v2x, double v2y, double v3x, double v3y) {
     v1 = new Point(v1x, v1y);
     v2 = new Point(v2x, v2y);
     v3 = new Point(v3x, v3y);
   }
+
 
   public Point getVertex(int x) {
     if (x == 1) {
@@ -25,6 +29,7 @@ public class Triangle {
     // }
   }
 
+
   public void setVertex(int x, Point where) {
     if (x == 1) {
       v1 = where;
@@ -37,23 +42,50 @@ public class Triangle {
     }
   }
 
+
   public String toString(){
     return "{" + v1 + ", " + v2 + ", " + v3 + "}";
   }
 
 
+  public double getPerimeter() {
+    double h1 = v1.distanceTo(v2);
+    double h2 = v2.distanceTo(v3);
+    double h3 = v3.distanceTo(v1);
+    return h1+h2+h3;
+  }
+
   // main
     public static void main(String[] args) {
+
       Point a = new Point(0, 0);
       Point b = new Point(0, 3);
-      Point c = new Point(4, 0);
-      Point d = new Point(6, 0);
+      Point c = new Point(6, 0);
+      Point d = new Point(4, 0);
       Triangle abc = new Triangle(a, b, c);
       Triangle xyz = new Triangle(1, 1, 1, 6, 13, 1);
+      Triangle def = new Triangle(0, 0, 0.5, 0, 0.5, Math.sqrt(3)/2);
+      Triangle jkl = new Triangle(1, 1, Math.sqrt(2)/2 + 1, 1, 1, Math.sqrt(2)/2 + 1);
+
+      System.out.println("\ntoString");
       System.out.println(abc);
       abc.setVertex(3, d);
       System.out.println(abc);
       System.out.println(xyz);
+      System.out.println(def);
+      System.out.println(jkl);
+
+      System.out.println("\ngetVertex");
+      System.out.println(abc.getVertex(2));
+      System.out.println(xyz.getVertex(1));
+      System.out.println(def.getVertex(3));
+      System.out.println(jkl.getVertex(2));
+
+      System.out.println("\ngetPerimeter");
+      System.out.println(abc.getPerimeter());
+      System.out.println(xyz.getPerimeter());
+      System.out.println(def.getPerimeter());
+      System.out.println(jkl.getPerimeter());
     }
 
 }
