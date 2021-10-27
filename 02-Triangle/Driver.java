65e2a3803ccb2966c7dbaae5bffca2deffb6be67
Jason Lin <jlin31@stuy.edu>
2021-10-27 17:47:14 -0400

    Test Cases

--- 02-Triangle/Driver.java
+++ 02-Triangle/Driver.java
@@ -25,19 +25,57 @@ public class Driver {
     System.out.println(origin.equals(zero1)); // false
     System.out.println(zero2.equals(zero3)); // true
     System.out.println(zero3.equals(zero4)); // false
-    System.out.println(origin.equals(null)); // false
 
+    System.out.println("\nTesting null exceptions");
+    System.out.println(origin.equals(null)); // false
+    Triangle abc2 = new Triangle(0, 0, 0, 3, 4, 0);
+    System.out.println(abc2.equals(null)); // false
 
     Point P1 = new Point(0, 3);
     Point P2 = new Point(4, 0);
     Triangle abc = new Triangle(origin, P1, P2);
-    Triangle abc2 = new Triangle(0, 0, 0, 3, 4, 0);
+
     System.out.println("\nTesting a 3/4/5 Triangle");
     System.out.println(abc);
     System.out.println(abc.equals(abc2));
     System.out.println(abc.classify());
     System.out.println(abc.area());
     System.out.println(abc.getPerimeter());
+
+    Triangle equal = new Triangle(0, 0, 3, 0, 1.5, 1.5*(Math.sqrt(3)));
+    Triangle cero = new Triangle(origin, origin, origin);
+    Triangle equal2 = new Triangle(0, 0, 2, 0, 1, Math.sqrt(3));
+    System.out.println("\nTesting an equilateral Triangle");
+    System.out.println(equal);
+    System.out.println(equal.getVertex(1));
+    System.out.println(equal.classify());
+    System.out.println(equal.area());
+
+    System.out.println("\n" + cero);
+    System.out.println(cero.getVertex(2));
+    System.out.println(cero.classify());
+    System.out.println(cero.getPerimeter());
+
+    System.out.println("\n" + equal2);
+    System.out.println(equal2.getVertex(3));
+    System.out.println(equal2.classify());
+    System.out.println(equal2.equals(equal)); // false
+
+    System.out.println("\nTesting an isosceles Triangle");
+    Triangle iso1 = new Triangle(0, 0, 0, 3, 3, 0);
+    Triangle iso2 = new Triangle(0, 0, 100, 0, 0, 100);
+    System.out.println(iso1.classify());
+    System.out.println(iso1.area());
+    System.out.println(iso2.classify());
+    System.out.println(iso2.getPerimeter());
+
+    System.out.println("\nTesting scalene triangle");
+    Triangle def2 = new Triangle(0, 0, 0.5, 0, 0.5, Math.sqrt(3)/2);
+    Triangle what = new Triangle(23, 42, 4823, 4239, 320, 0);
+    System.out.println(def2.classify());
+    System.out.println(def2.area());
+    System.out.println(what.classify());
+    System.out.println(what.getPerimeter());
 
 
   }
