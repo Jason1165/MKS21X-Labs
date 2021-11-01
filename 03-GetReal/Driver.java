9c608abf26bf4da95301c98c6e9a4b625188da01
Jason Lin <jlin31@stuy.edu>
2021-11-01 17:33:25 -0400

    Added test cases for RealNumber

--- 03-GetReal/Driver.java
+++ 03-GetReal/Driver.java
@@ -3,7 +3,19 @@ public class Driver {
   public static void main(String[] args) {
     RealNumber x = new RealNumber(1);
     RealNumber y = new RealNumber(2);
-    x.add(y);
+    RealNumber z = new RealNumber(3.5);
 
+    System.out.println("\nTesting 1 and 2");
+    System.out.println(x.add(y));
+    System.out.println(x.subtract(y));
+    System.out.println(x.divide(y));
+
+    System.out.println("\nTesting 'squares'");
+    System.out.println(x.multiply(x));
+    System.out.println(y.multiply(y));
+
+    System.out.println("\nTesting division");
+    System.out.println(z.divide(x));
+    System.out.println(z.divide(z));
   }
 }
