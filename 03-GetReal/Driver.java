8988762440ba6cda7dfc2eca7ad332cd4212e278
Jason Lin <jlin31@stuy.edu>
2021-11-02 14:40:01 -0400

    Test Cases for all methods

--- 03-GetReal/Driver.java
+++ 03-GetReal/Driver.java
@@ -5,24 +5,24 @@ public class Driver {
     RealNumber y = new RealNumber(2);
     RealNumber z = new RealNumber(3.5);
 
-    //
-    // System.out.println("\nTesting 1 and 2");
-    // System.out.println(x.add(y));
-    // System.out.println(x.subtract(y));
-    // System.out.println(x.divide(y));
-    //
-    // System.out.println("\nTesting 'squares'");
-    // System.out.println(x.multiply(x));
-    // System.out.println(y.multiply(y));
-    //
-    // System.out.println("\nTesting division");
-    // System.out.println(z.divide(x));
-    // System.out.println(z.divide(z));
-    //
-    // System.out.println("\nTesting toString");
-    // System.out.println(x);
-    // System.out.println(y);
-    // System.out.println(z);
+
+    System.out.println("\nTesting 1 and 2");
+    System.out.println(x.add(y));
+    System.out.println(x.subtract(y));
+    System.out.println(x.divide(y));
+
+    System.out.println("\nTesting 'squares'");
+    System.out.println(x.multiply(x));
+    System.out.println(y.multiply(y));
+
+    System.out.println("\nTesting division");
+    System.out.println(z.divide(x));
+    System.out.println(z.divide(z));
+
+    System.out.println("\nTesting toString");
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println(z);
 
     RationalNumber R1 = new RationalNumber(100, 500);
     RationalNumber R2 = new RationalNumber(10, 50);
@@ -34,54 +34,54 @@ public class Driver {
     RationalNumber R8 = new RationalNumber(2, 5);
     RationalNumber R9 = new RationalNumber(8, 5);
 
-    // System.out.println("\nTesting toString for RationalNumber and reduction");
-    // System.out.println(R1);
-    // System.out.println(R2);
-    // System.out.println(R3);
-    // System.out.println(R4);
-    // System.out.println(R5); // constructor correct
-    // System.out.println("\nTesting reciprocal");
-    // System.out.println(R1.reciprocal());
-    // System.out.println(R2.reciprocal());
-    // System.out.println(R3.reciprocal());
-    // System.out.println(R4.reciprocal());
-    // System.out.println(R5.reciprocal());
-    // System.out.println("\nTesting equality");
-    // System.out.println(R1.equals(R2));
-    // System.out.println(R1.equals(R3));
-    // System.out.println(R4.equals(R5));
-    // System.out.println("\nTesting addition");
-    // System.out.println(R6.add(R3));
-    // System.out.println(R2.add(R3));
-    // System.out.println(R4.add(R6));
-    // System.out.println(R7.add(R8));
-    // System.out.println("\nTesting subtraction");
-    // System.out.println(R3.subtract(R6));
-    // System.out.println(R2.subtract(R3));
-    // System.out.println(R4.subtract(R6));
-    // System.out.println(R7.subtract(R8));
-    // System.out.println("\nTesting division");
-    // // System.out.println(R4.divide(R5));
-    // System.out.println(R1.divide(R2));
-    // System.out.println(R9.divide(R8));
-    // System.out.println(R8.divide(R7));
-    // System.out.println("\nTesting multiplication");
-    // System.out.println(R1.multiply(R4));
-    // System.out.println(R8.multiply(R9));
-    // System.out.println(R3.multiply(R7));
+    System.out.println("\nTesting toString for RationalNumber and reduction");
+    System.out.println(R1);
+    System.out.println(R2);
+    System.out.println(R3);
+    System.out.println(R4);
+    System.out.println(R5); // constructor correct
+    System.out.println("\nTesting reciprocal");
+    System.out.println(R1.reciprocal());
+    System.out.println(R2.reciprocal());
+    System.out.println(R3.reciprocal());
+    System.out.println(R4.reciprocal());
+    System.out.println(R5.reciprocal());
+    System.out.println("\nTesting equality");
+    System.out.println(R1.equals(R2));
+    System.out.println(R1.equals(R3));
+    System.out.println(R4.equals(R5));
+    System.out.println("\nTesting addition");
+    System.out.println(R6.add(R3));
+    System.out.println(R2.add(R3));
+    System.out.println(R4.add(R6));
+    System.out.println(R7.add(R8));
+    System.out.println("\nTesting subtraction");
+    System.out.println(R3.subtract(R6));
+    System.out.println(R2.subtract(R3));
+    System.out.println(R4.subtract(R6));
+    System.out.println(R7.subtract(R8));
+    System.out.println("\nTesting division");
+    // System.out.println(R4.divide(R5));
+    System.out.println(R1.divide(R2));
+    System.out.println(R9.divide(R8));
+    System.out.println(R8.divide(R7));
+    System.out.println("\nTesting multiplication");
+    System.out.println(R1.multiply(R4));
+    System.out.println(R8.multiply(R9));
+    System.out.println(R3.multiply(R7));
     RealNumber N1 = new RealNumber(10);
     RealNumber N2 = new RationalNumber(10, 1);
     RealNumber N3 = new RationalNumber(2, 1);
     RealNumber N4 = new RationalNumber(22, 7);
-    // System.out.println("\nTesting extensions");
-    // System.out.println(N1);
-    // System.out.println(N2);
-    // System.out.println(N4);
-    // System.out.println(N4.getValue());
-    // System.out.println(N1.equals(N2));
-    // System.out.println(N1.equals(N2.getValue()));
-    // System.out.println(N1.add(N2));
-    // System.out.println(N1.divide(N2));
+    System.out.println("\nTesting extensions");
+    System.out.println(N1);
+    System.out.println(N2);
+    System.out.println(N4);
+    System.out.println(N4.getValue());
+    System.out.println(N1.equals(N2));
+    System.out.println(N1.equals(N2.getValue()));
+    System.out.println(N1.add(N2));
+    System.out.println(N1.divide(N2));
     System.out.println(N1.equals(null));
     System.out.println(N2.equals(null));
     // System.out.println(N3.add(null));
