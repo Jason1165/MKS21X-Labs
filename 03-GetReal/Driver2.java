a647da1831b835ce489d670c5758d4371ef8f61a
Jason Lin <jlin31@stuy.edu>
2021-11-03 17:20:56 -0400

    Additional Tests

--- 03-GetReal/Driver2.java
+++ 03-GetReal/Driver2.java
@@ -3,8 +3,8 @@ public class Driver2 {
   public static void main(String[] args) {
     System.out.println("\nTesting Randomness");
     for (int i = 0; i < 5; i++) {
-      int a = 0;
-      int b = 4;
+      int a = -10;
+      int b = 10;
       int value = (int)(Math.random()*(b-a+1)+a);
       int value2 = (int)(Math.random()*(b-a+1)+a);
       RationalNumber R1 = new RationalNumber(value, value2);
@@ -12,10 +12,11 @@ public class Driver2 {
       RealNumber vx = new RealNumber(value);
       RealNumber v2 = new RealNumber(value2);
       System.out.println("v1: " + value + "\t\tv2: " + value2);
-      System.out.println("R1: " + R1 + "\t\tR2: " + R2);
+      System.out.print("R1: " + R1 + "\t\tR2: " + R2);
       System.out.println("compareTo 1v2: " + R1.compareTo(R2));
       System.out.println("compareTo v1 v2: " + vx.compareTo(v2));
       System.out.println("compare to v1 R1: " + vx.compareTo(R1));
+      System.out.println("R1 equals R2: " + R1.equals(R2));
       System.out.println();
     }
   }
