80dcf556929950b39b3aa5f169fb6e0fe6cb7972
Jason Lin <jlin31@stuy.edu>
2021-11-03 16:24:31 -0400

    Tested Random in range of 0-99 inclusive

--- 03-GetReal/Driver2.java
+++ 03-GetReal/Driver2.java
@@ -1,5 +1,11 @@
 public class Driver2 {
   public static void main(String[] args) {
-    System.out.println();
+    System.out.println("\nTesting Randomness");
+    for (int i = 0; i < 5; i++) {
+      int value = (int)(Math.random()*(100));
+      int value2 = (int)(Math.random()*(100));
+      RationalNumber R1 = new RationalNumber(value, value2);
+      System.out.println("value: " + value + "\tvalue2: " + value2 + "\tRational: " + R1);
+    }
   }
 }
