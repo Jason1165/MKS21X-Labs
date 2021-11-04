3aca437b363c081c4e64df34652d49e4dfc6be28
Jason Lin <jlin31@stuy.edu>
2021-11-04 17:19:19 -0400

    Test cases for toString and toStringDebug

--- 04-SuperArray/Tester.java
+++ 04-SuperArray/Tester.java
@@ -1,5 +1,7 @@
 public class Tester {
   public static void main(String[] args) {
-
+    SuperArray SA = new SuperArray();
+    System.out.println(SA);
+    System.out.println(SA.toStringDebug());
   }
 }
