534c866218cfa8f330487d660875807f3dcdad4a
Jason Lin <jlin31@stuy.edu>
2021-11-05 14:46:06 -0400

    Tested set and get as well as fixed error message to make it clearer for me and fixed for loop code

--- 04-SuperArray/Tester.java
+++ 04-SuperArray/Tester.java
@@ -4,22 +4,46 @@ public class Tester {
     System.out.println(SA);
     System.out.println(SA.toStringDebug());
     System.out.println(SA.size());
+    String[] x = {"not zero", "not one", "not two", "not three", "not for", "not fi", "not se", "not sev"};
 
-    SA.add("print");
-    SA.add("hello");
-    SA.add("world");
-    SA.add("why");
-    SA.add("not food");
-    SA.add("more");
-    SA.add("max");
+    SA.add("zero");
+    SA.add("one");
+    SA.add("two");
+    SA.add("three");
+    SA.add("four");
+    SA.add("five");
+    SA.add("six");
     System.out.println(SA);
     System.out.println(SA.toStringDebug());
     System.out.println(SA.size());
+    System.out.println("\nPhase Two Testing");
+    System.out.println("\nGet all the Values");
+    for (int i = 0; i < SA.size(); i++) {
+      System.out.println(SA.get(i));
+    }
 
-    SA.remove(2);
-    System.out.println(SA);
-    System.out.println(SA.toStringDebug());
-    System.out.println(SA.size());
+    System.out.println("\nSet all the values");
+    for (int i = 0; i < SA.size(); i++){
+      SA.set(i, x[i+1]);
+    }
+
+    for (int i = 0; i < SA.size(); i++) {
+      System.out.println(SA.get(i));
+    }
+
+    System.out.println("\nRight shift one");
+    for (int i = 0; i < SA.size()-1; i++) {
+      SA.set(i, SA.get(i+1));
+      System.out.println(SA.get(i));
+    }
+
+    SA.set(SA.size()-1, "NOPE!");
+    System.out.println(SA.toString());
+
+    // SA.remove(2);
+    // System.out.println(SA);
+    // System.out.println(SA.toStringDebug());
+    // System.out.println(SA.size());
 
     // SA.add("null");
     // System.out.println(SA);
