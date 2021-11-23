019524619fb9fcd1b978618e94e24cdbd5f7c40b
Jason Lin <jlin31@stuy.edu>
2021-11-23 14:27:46 -0500

    Wrapping stub

---
+++ 06-Advent/Wrapping.java
@@ -0,0 +1,12 @@
+public class Wrapping {
+  public static void main(String[] args) {
+    if (args[1].equals("1")){
+      part1(args[0]);
+    }
+  }
+
+  public static void part1(String file){
+
+  }
+
+}
