9d3951f6698ad0e269339934008915e3461f4b57
Jason Lin <jlin31@stuy.edu>
2021-11-23 14:42:52 -0500

    Part2

--- 06-Advent/Wrapping.java
+++ 06-Advent/Wrapping.java
@@ -8,6 +8,9 @@ public class Wrapping {
     if (args[1].equals("1")){
       part1(args[0]);
     }
+    if (args[1].equals("2")) {
+      part2(args[0]);
+    }
   }
 
   public static void part1(String file){
@@ -38,6 +41,28 @@ public class Wrapping {
       ex.printStackTrace();
     }
 
+    System.out.println(total);
+  }
+
+  public static void part2(String file) {
+    int total = 0;
+    try {
+      File f = new File(file);
+      Scanner in = new Scanner(f);
+      while (in.hasNextLine()) {
+        String x = in.nextLine();
+        String[]nums = x.split("x");
+        int[]nums1 = new int[3];
+        nums1[0] = Integer.parseInt(nums[0]);
+        nums1[1] = Integer.parseInt(nums[1]);
+        nums1[2] = Integer.parseInt(nums[2]);
+        Arrays.sort(nums1);
+
+        total += (nums1[0]*2) + (nums1[1]*2) + (nums1[0]*nums1[2]*nums1[1]);
+      }
+    } catch (FileNotFoundException ex) {
+      ex.printStackTrace();
+    }
     System.out.println(total);
   }
 
