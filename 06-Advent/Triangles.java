433e5badc6066a419e6405344dfd6615dac3f36f
Jason Lin <jlin31@stuy.edu>
2021-11-23 13:29:26 -0500

    Part2 Fixed and ARGS

--- 06-Advent/Triangles.java
+++ 06-Advent/Triangles.java
@@ -4,16 +4,20 @@ import java.util.Scanner;
 
 public class Triangles {
   public static void main(String[] args) {
-    System.out.println(Triangle1());
-    System.out.println(Triangle2());
+    if (args[1].equals("1")) {
+      Triangle1(args[0]);
+    }
+    if (args[1].equals("2")) {
+      Triangle2(args[0]);
+    }
 
   }
 
-  public static int Triangle1() {
+  public static void Triangle1(String argss) {
     int num = 0;
 
     try {
-      File file = new File("Triangle.txt");
+      File file = new File(argss);
       Scanner input = new Scanner(file);
       while (input.hasNextInt()){
         int a = input.nextInt();
@@ -30,33 +34,34 @@ public class Triangles {
             //File not found
       }
 
-  return num;
+  System.out.println(num);
   }
 
-  public static int Triangle2(){
+  public static void Triangle2(String argggs){
     int num = 0;
     try{
-      File file = new File("Triangle.txt");
+      File file = new File(argggs);
       Scanner input = new Scanner(file);
       while (input.hasNextInt()){
         int a1 = input.nextInt();
         int b1 = input.nextInt();
         int c1 = input.nextInt();
-        if(a1+b1>c1 && a1+c1>b1 && c1+b1>a1) {
-          num++;
-        }
+
 
         int a2 = input.nextInt();
         int b2 = input.nextInt();
         int c2 = input.nextInt();
-        if(a2+b2>c2 && a2+c2>b2 && c2+b2>a2) {
-          num++;
-        }
 
         int a3 = input.nextInt();
         int b3 = input.nextInt();
         int c3 = input.nextInt();
-        if(a3+b3>c3 && a3+c3>b3 && c3+b3>a3) {
+        if(a1+a2>a3 && a1+a3>a2 && a2+a3>a1) {
+          num++;
+        }
+        if(b1+b2>b3 && b2+b3>b1 && b3+b1>b2) {
+          num++;
+        }
+        if(c1+c2>c3 && c1+c3>c2 && c2+c3>c1) {
           num++;
         }
 
@@ -66,7 +71,7 @@ public class Triangles {
       //File not found
     }
 
-    return num;
+    System.out.println(num);
   }
 
 }
