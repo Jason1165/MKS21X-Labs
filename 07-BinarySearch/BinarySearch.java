7977a6bae474de562c29e5dbdbc59eb527e61f80
Jason Lin <jlin31@stuy.edu>
2021-11-29 16:01:08 -0500

    Working version that passes all sample test cases.

--- 07-BinarySearch/BinarySearch.java
+++ 07-BinarySearch/BinarySearch.java
@@ -2,6 +2,41 @@ import java.util.Arrays;
 
 public class BinarySearch {
   public static void main(String[] args) {
-
+    int result;
+    result = binarySearch(new int[]{0,0,0,0,0,0,0},0);
+    System.out.println(result);//should be 3
+    result = binarySearch(new int[]{0,0,0,0,0,0,0},1);
+    System.out.println(result);//should be -1
+    result = binarySearch(new int[]{0,1,2,3,4,5,6},0);
+    System.out.println(result);//should be 0
+    //...
+    result = binarySearch(new int[]{0,1,2,3,4,5,6},6);
+    System.out.println(result);//should be 6
   }
+
+  public static int binarySearch(int[] data, int target) {
+    int min = 0;
+    int max = data.length-1;
+    int mid = (min+max)/2;
+    while (min <= max) {
+      if (data[mid] == target) {
+        return mid;
+      }
+      else if (target < data[mid]) {
+        System.out.println("Greater: " + min + "\t" + mid + "\t" + max);
+        max = mid-1;
+        mid = (min+max)/2;
+        System.out.println("Greater: " + min + "\t" + mid + "\t" + max);
+      }
+      else if (target > data[mid]) {
+        System.out.println("Lesser: " + min + "\t" + mid + "\t" + max);
+        min = mid+1;
+        mid = (min+max)/2;
+        System.out.println("Lesser: " + min + "\t" + mid + "\t" + max);
+      }
+
+    }
+    return -1;
+  }
+
 }
