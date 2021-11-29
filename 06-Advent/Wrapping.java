74737395564e8081892ab027c68494b09ef11e23
Jason Lin <jlin31@stuy.edu>
2021-11-29 13:26:04 -0500

    File Rename

--- 06-Advent/Wrapping.java
+++ 06-Advent/Wrapping.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class Wrapper {
+public class Wrapping {
   public static void main(String[] args) {
     if (args[1].equals("1")){
       part1(args[0]);
