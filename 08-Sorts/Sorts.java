e71056ac9771380009eeaa9ea92750f1916a9c83
Jason Lin <jlin31@stuy.edu>
2021-12-01 13:46:40 -0500

    Test Cases

--- 08-Sorts/Sorts.java
+++ 08-Sorts/Sorts.java
@@ -1,5 +1,15 @@
 public class Sorts{
-
+  public static void main(String[] args) {
+    int[] test1 = {5, 1 , 12, -5, 16};
+    int[] test2 = {2, 3, 4, 5, 1};
+    int[] test3 = {6, 1, 2, 3, 4, 5};
+    int[] test4 = {0, 1, 2, -1, 1, 2};
+    bubbleSort(test1);
+    bubbleSort(test2);
+    bubbleSort(test3);
+    bubbleSort(test4);
+  }
+  
   /**Bubble sort of an int array.
   *Upon completion, the elements of the array will be in increasing order.
   *@param data  the elements to be sorted.
