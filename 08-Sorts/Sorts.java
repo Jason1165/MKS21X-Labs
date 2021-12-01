da09532c14915a12db17995bd9840349f7df32d1
Jason Lin <jlin31@stuy.edu>
2021-12-01 13:55:48 -0500

    Bubble Sort Working, Does not stop early

--- 08-Sorts/Sorts.java
+++ 08-Sorts/Sorts.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Sorts{
   public static void main(String[] args) {
     int[] test1 = {5, 1 , 12, -5, 16};
@@ -9,12 +10,29 @@ public class Sorts{
     bubbleSort(test3);
     bubbleSort(test4);
   }
-  
+
   /**Bubble sort of an int array.
   *Upon completion, the elements of the array will be in increasing order.
   *@param data  the elements to be sorted.
   */
   public static void bubbleSort(int[] data){
-
+    boolean inOrder = false;
+    while (!inOrder) {
+      for (int i = data.length; i > 0; i--) {
+        inOrder = true;
+        for (int k = 0; k < i-1; k++) {
+          if (data[k] > data[k+1]) {
+            int x = data[k];
+            int y = data[k+1];
+            data[k] = y;
+            data[k+1] = x;
+            inOrder = false;
+          }
+          // System.out.println(Arrays.toString(data));
+        }
+        System.out.println(Arrays.toString(data));
+      }
+    }
   }
+
 }
