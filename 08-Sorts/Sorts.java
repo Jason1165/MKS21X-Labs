eec3b0757c07b63d7415f657bf966fd133cf2803
Jason Lin <jlin31@stuy.edu>
2021-12-02 14:38:01 -0500

    Testing random values in increasing sizes of 0 - 1000

--- 08-Sorts/Sorts.java
+++ 08-Sorts/Sorts.java
@@ -1,24 +1,39 @@
 import java.util.Arrays;
 public class Sorts{
   public static void main(String[] args) {
-    int[] test1 = {5, 1 , 12, -5, 16};
-    int[] test2 = {2, 3, 4, 5, 1};
-    int[] test3 = {6, 1, 2, 3, 4, 5};
-    int[] test4 = {0, 1, 2, -1, 1, 2};
-    int[] test5 = {};
-    int[] test6 = {1};
-    bubbleSort(test1);
-    bubbleSort(test2);
-    bubbleSort(test3);
-    bubbleSort(test4);
-    bubbleSort(test5);
-    bubbleSort(test6);
-    int[] ary = new int[100];
-    for (int i = 0; i < 100; i++) {
-      ary[i] = (int)(Math.random()*100)-50;
+    // int[] test1 = {5, 1 , 12, -5, 16};
+    // int[] test2 = {2, 3, 4, 5, 1};
+    // int[] test3 = {6, 1, 2, 3, 4, 5};
+    // int[] test4 = {0, 1, 2, -1, 1, 2};
+    // int[] test5 = {};
+    // int[] test6 = {1};
+    // bubbleSort(test1);
+    // bubbleSort(test2);
+    // bubbleSort(test3);
+    // bubbleSort(test4);
+    // bubbleSort(test5);
+    // bubbleSort(test6);
+
+    // Size Sort
+    for (int i = 0; i <= 1000; i++) {
+      int[]aryTest = new int[i];
+      int[]ary = new int[i];
+      for (int j = 0; j < i; j++) {
+        int x = (int)(Math.random()*100)-50;
+        aryTest[j] = x;
+        ary[j] = x;
+      }
+      Arrays.sort(aryTest);
+      bubbleSort(ary);
+      if (!isEqual(ary, aryTest)) {
+        System.out.println(Arrays.toString(ary));
+        System.out.println(Arrays.toString(aryTest));
+        System.out.println("Error");
+        System.out.println("Size: " + i);
+        System.exit(1);
+      }
     }
-    System.out.println(Arrays.toString(ary));
-    bubbleSort(ary);
+    System.out.println("Size Test Completely");
   }
 
   /**Bubble sort of an int array.
@@ -43,7 +58,16 @@ public class Sorts{
         i--;
         // System.out.println(Arrays.toString(data));
     }
-    System.out.println(Arrays.toString(data));
+    // System.out.println(Arrays.toString(data));
   }
 
+
+  public static boolean isEqual(int[] ary1, int[] ary2) {
+    for (int i = 0; i < ary1.length; i++) {
+      if (ary1[i] != ary2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
