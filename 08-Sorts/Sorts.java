f63c205823dfc856e32c68371cb59169021145c0
Jason Lin <jlin31@stuy.edu>
2021-12-06 16:03:01 -0500

    initial tests

--- 08-Sorts/Sorts.java
+++ 08-Sorts/Sorts.java
@@ -3,127 +3,117 @@ public class Sorts{
   public static void main(String[] args) {
 
     int times = Integer.parseInt(args[1]);
-    if (args[0].equals("0")) {
+    if (args[0].equals("bubble")) {
       // Size Sort
-      for (int i = 0; i <= times; i++) {
-        int[]aryTest = new int[i];
-        for (int j = 0; j < i; j++) {
+      int[]aryTest = new int[times];
+      for (int i = 0; i < times; i++) {
           int x = (int)(Math.random()*100)-50;
-          aryTest[j] = x;
+          aryTest[i] = x;
         }
         int[]ary = Arrays.copyOfRange(aryTest, 0, aryTest.length);
         Arrays.sort(aryTest);
         bubbleSort(ary);
-        test(ary, aryTest, i);
-      }
+        test(ary, aryTest);
       System.out.println("BubbleSort: Random Complete");
 
       // same sort
-      for (int i = 0; i <= times; i++) {
-        int[]aryTest = new int[i];
-        int x = (int)(Math.random()*100)-50;
-        for (int j = 0; j < i; j++) {
-          aryTest[j] = x;
+      int[]aryTest1 = new int[times];
+      int x = (int)(Math.random()*100)-50;
+      for (int i = 0; i < times; i++) {
+          aryTest1[i] = x;
         }
-        int[]ary = Arrays.copyOfRange(aryTest, 0, aryTest.length);
-        Arrays.sort(aryTest);
-        bubbleSort(ary);
-        test(ary, aryTest, i);
-      }
+        int[]ary1 = Arrays.copyOfRange(aryTest1, 0, aryTest1.length);
+        Arrays.sort(aryTest1);
+        bubbleSort(ary1);
+        test(ary1, aryTest1);
       System.out.println("BubbleSort: Same Complete");
 
       // reverse sort
-      for (int i = 0; i <= times; i++) {
-        int[]aryTest = new int[i];
-        for (int j = 0; j < i; j++) {
-          int x = (int)(Math.random()*100)-50;
-          aryTest[j] = x;
+      int[]aryTest2 = new int[times];
+      for (int i = 0; i < times; i++) {
+          int y = (int)(Math.random()*100)-50;
+          aryTest2[i] = y;
         }
-        int[]ary = Arrays.copyOfRange(aryTest, 0, aryTest.length);
-        aryTest = reverse(aryTest);
-        ary = reverse(ary);
-        Arrays.sort(aryTest);
-        bubbleSort(ary);
-        test(ary, aryTest, i);
-      }
+        int[]ary2 = Arrays.copyOfRange(aryTest2, 0, aryTest2.length);
+        aryTest2 = reverse(aryTest2);
+        ary2 = reverse(ary2);
+        Arrays.sort(aryTest2);
+        bubbleSort(ary2);
+        test(ary2, aryTest2);
       System.out.println("BubbleSort: Reverse Complete");
 
       // Increase sort
-      for (int i = 0; i <= times; i++) {
-        int[]aryTest = new int[i];
-        for (int j = 0; j < i; j++) {
-          int x = (int)(Math.random()*100)-50;
-          aryTest[j] = x;
+      int[]aryTest3 = new int[times];
+      for (int i = 0; i < times; i++) {
+          int y = (int)(Math.random()*100)-50;
+          aryTest[i] = y;
         }
-        int[]ary = Arrays.copyOfRange(aryTest, 0, aryTest.length);
-        Arrays.sort(aryTest);
-        Arrays.sort(ary);
-        bubbleSort(ary);
-        test(ary, aryTest, i);
-      }
+        int[]ary3 = Arrays.copyOfRange(aryTest3, 0, aryTest3.length);
+        Arrays.sort(aryTest3);
+        Arrays.sort(ary3);
+        bubbleSort(ary3);
+        test(ary3, aryTest3);
       System.out.println("BubbleSort: Increase Complete");
     }
 
 
-    if (args[0].equals("1")) {
+    if (args[0].equals("selection")) {
       // Size Sort
-      for (int i = 0; i <= times; i++) {
-        int[]aryTest = new int[i];
-        for (int j = 0; j < i; j++) {
+      int[]aryTest = new int[times];
+      for (int i = 0; i < times; i++) {
           int x = (int)(Math.random()*100)-50;
-          aryTest[j] = x;
+          aryTest[i] = x;
         }
         int[]ary = Arrays.copyOfRange(aryTest, 0, aryTest.length);
         Arrays.sort(aryTest);
         selectionSort(ary);
-        test(ary, aryTest, i);
-      }
+        test(ary, aryTest);
       System.out.println("selectionSort: Random Complete");
 
       // same sort
-      for (int i = 0; i <= times; i++) {
-        int[]aryTest = new int[i];
-        int x = (int)(Math.random()*100)-50;
-        for (int j = 0; j < i; j++) {
-          aryTest[j] = x;
+      int[]aryTest1 = new int[times];
+      int x = (int)(Math.random()*100)-50;
+      for (int i = 0; i < times; i++) {
+          aryTest1[i] = x;
         }
-        int[]ary = Arrays.copyOfRange(aryTest, 0, aryTest.length);
-        Arrays.sort(aryTest);
-        selectionSort(ary);
-        test(ary, aryTest, i);
-      }
+        int[]ary1 = Arrays.copyOfRange(aryTest1, 0, aryTest1.length);
+        Arrays.sort(aryTest1);
+        selectionSort(ary1);
+        test(ary1, aryTest1);
       System.out.println("selectionSort: Same Complete");
 
       // reverse sort
-      for (int i = 0; i <= times; i++) {
-        int[]aryTest = new int[i];
-        for (int j = 0; j < i; j++) {
-          int x = (int)(Math.random()*100)-50;
-          aryTest[j] = x;
+      int[]aryTest2 = new int[times];
+      for (int i = 0; i < times; i++) {
+          int y = (int)(Math.random()*100)-50;
+          aryTest2[i] = y;
         }
-        int[]ary = Arrays.copyOfRange(aryTest, 0, aryTest.length);
-        aryTest = reverse(aryTest);
-        ary = reverse(ary);
-        Arrays.sort(aryTest);
-        selectionSort(ary);
-        test(ary, aryTest, i);
-      }
+        int[]ary2 = Arrays.copyOfRange(aryTest2, 0, aryTest2.length);
+        aryTest2 = reverse(aryTest2);
+        ary2 = reverse(ary2);
+        Arrays.sort(aryTest2);
+        selectionSort(ary2);
+        test(ary2, aryTest2);
       System.out.println("selectionSort: Reverse Complete");
 
       // Increase sort
-      for (int i = 0; i <= times; i++) {
-        int[]aryTest = new int[i];
-        for (int j = 0; j < i; j++) {
-          int x = (int)(Math.random()*100)-50;
-          aryTest[j] = x;
+      int[]aryTest3 = new int[times];
+      for (int i = 0; i < times; i++) {
+          int y = (int)(Math.random()*100)-50;
+          aryTest[i] = y;
         }
-        int[]ary = Arrays.copyOfRange(aryTest, 0, aryTest.length);
-        Arrays.sort(aryTest);
-        Arrays.sort(ary);
-        selectionSort(ary);
-        test(ary, aryTest, i);
-      }
+        int[]ary3 = Arrays.copyOfRange(aryTest3, 0, aryTest3.length);
+        Arrays.sort(aryTest3);
+        Arrays.sort(ary3);
+        selectionSort(ary3);
+        test(ary3, aryTest3);
       System.out.println("selectionSort: Increase Complete");
+    }
+
+    else {
+      int[] ary = {1, 2, 3, 1};
+      insertionSort(ary);
+      System.out.println(Arrays.toString(ary));
     }
   }
 
@@ -170,6 +160,21 @@ public class Sorts{
     // System.out.println(Arrays.toString(ary));
   }
 
+  public static void insertionSort(int[] data) {
+    for (int i = 1; i < data.length; i++ ) {
+      if (data[i] < data[i-1]) {
+        System.out.println();
+        int x = data[i];
+        int j = i-1;
+        while(data[j] > x && j >= 0 ) {
+          data[j+1] = data[j];
+          j--;
+        }
+        data[j]= x;
+      }
+    }
+  }
+
   public static boolean isEqual(int[] ary1, int[] ary2) {
     for (int i = 0; i < ary1.length; i++) {
       if (ary1[i] != ary2[i]) {
@@ -187,12 +192,11 @@ public class Sorts{
     return ans;
   }
 
-  public static void test(int[] ary, int[] aryTest, int i) {
+  public static void test(int[] ary, int[] aryTest) {
     if (!isEqual(ary, aryTest)) {
       System.out.println(Arrays.toString(ary));
       System.out.println(Arrays.toString(aryTest));
       System.out.println("Error");
-      System.out.println("Size: " + i);
       System.exit(1);
     }
   }
