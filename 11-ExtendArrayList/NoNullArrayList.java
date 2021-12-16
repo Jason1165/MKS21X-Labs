718f171542e2bdae9501c928ee504a9ec40eee5b
Jason Lin <jlin31@stuy.edu>
2021-12-16 14:41:10 -0500

    Override methods

--- 11-ExtendArrayList/NoNullArrayList.java
+++ 11-ExtendArrayList/NoNullArrayList.java
@@ -8,4 +8,28 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   public NoNullArrayList(int initialCapacity) {
     super(initialCapacity);
   }
+
+  public boolean add (T e) {
+    if (e == null) {
+      throw new IllegalArgumentException("IllegalArgumentException");
+    }
+    boolean bool = super.add(e);
+    return bool;
+  }
+
+  public void add(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("IllegalArgumentException");
+    }
+    super.add(index, element);
+  }
+
+  public T set(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("IllegalArgumentException");
+    }
+    T bool = super.set(index, element);
+    return bool;
+  }
+
 }
