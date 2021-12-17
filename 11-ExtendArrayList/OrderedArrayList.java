b0a81d95feea3a3abebc539444348539a06eba4f
Jason Lin <jlin31@stuy.edu>
2021-12-17 14:48:48 -0500

    whereToPlace functioning

--- 11-ExtendArrayList/OrderedArrayList.java
+++ 11-ExtendArrayList/OrderedArrayList.java
@@ -9,7 +9,7 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super(startingCapacity);
   }
 
-  public int whereToPlace(T value){
+  private int whereToPlace(T value){
     /*return the index that the value should be placed
     when inserting into the OrderedArrayList .*/
     int loc = 0;
@@ -18,21 +18,13 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     }
     else {
       for (int i = 0; i < this.size(); i++) {
-        if (value.compareTo(this.get(i)) == -1) {
+        if (value.compareTo(this.get(i)) > 0) {
+          // System.out.println(i);
           loc = i;
         }
       }
     }
-    return loc;
-  }
-
-  public static void main(String[] args) {
-    OrderedArrayList<String> test = new OrderedArrayList<String>();
-    for (int i = 0; i < 26; i++) {
-      String x = "" + (char)(i+65);
-      test.add(x);
-    }
-    System.out.println(test.toString());
+    return loc+1;
   }
 
 }
