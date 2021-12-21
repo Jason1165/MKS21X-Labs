62c34287b2b2da15ffd4c85c0657770fd7149cf0
Jason Lin <jlin31@stuy.edu>
2021-12-21 16:39:35 -0500

    Slight change in wording for exception message

--- 11-ExtendArrayList/NoNullArrayList.java
+++ 11-ExtendArrayList/NoNullArrayList.java
@@ -13,7 +13,7 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   @Override
   public boolean add (T e) {
     if (e == null) {
-      throw new IllegalArgumentException("Cannot be a null");
+      throw new IllegalArgumentException("Value cannot be a null");
     }
     else {
       return super.add(e);
@@ -23,7 +23,7 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   @Override
   public void add(int index, T element) {
     if (element == null) {
-      throw new IllegalArgumentException("Cannot be a null");
+      throw new IllegalArgumentException("Value cannot be a null");
     }
     else {
       super.add(index, element);
@@ -33,7 +33,7 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   @Override
   public T set(int index, T element) {
     if (element == null) {
-      throw new IllegalArgumentException("Cannot be a null");
+      throw new IllegalArgumentException("Value cannot be a null");
     }
     else {
       return super.set(index, element);
