242e862e61ff4d20e31136a5ceb94b1ee2c51f40
Jason Lin <jlin31@stuy.edu>
2022-01-12 14:36:06 -0500

    Working Border

--- 12-Stuyablo/Game.java
+++ 12-Stuyablo/Game.java
@@ -1,18 +1,37 @@
-public class Game{
+import java.util.*;
+public class Game {
+  public static final int WIDTH = 80;
+  public static final int HEIGHT = 30;
+  public static final int TEXTCOLOR = Text.WHITE;
+  public static final int TEXTBACKGROUND = Text.BLUE + Text.BACKGROUND;
+
   public static void main(String[] args) {
-    Adventurer a = new Warrior("Conan","Aaaaaaaar!",20);
-    Adventurer b = new Wizard("Merlin", 2 , 500);
-    System.out.println(a+" ("+a.getHP()+"HP)");
-    System.out.println(b+" ("+b.getHP()+"HP)");
-    a.attack(b);
-    b.attack(a);
-    System.out.println(a+" ("+a.getHP()+"HP)");
-    System.out.println(b+" ("+b.getHP()+"HP)");
-    a.specialAttack(b);
-    b.specialAttack(a);
-    System.out.println(a+" ("+a.getHP()+"HP)");
-    System.out.println(b+" ("+b.getHP()+"HP)");
+    Text.hideCursor();
+    Text.clear();
+    createBorder(HEIGHT, WIDTH);
+    Text.go(HEIGHT+1, 1);
+  }
 
+  public static void borderVertical(int len) {
+    for (int i = 1 ; i <= len; i++) {
+      Text.go(i, 1);
+      System.out.print(Text.colorize(" ", TEXTBACKGROUND));
+      Text.go(i, WIDTH);
+      System.out.print(Text.colorize(" ", TEXTBACKGROUND));
     }
+  }
 
+  public static void borderHorizontal(int len) {
+    for (int i = 1 ; i <= len; i++) {
+      Text.go(1, i);
+      System.out.print(Text.colorize(" ", TEXTBACKGROUND));
+      Text.go(HEIGHT, i);
+      System.out.print(Text.colorize(" ", TEXTBACKGROUND));
+    }
+  }
+
+  public static void createBorder(int x, int y) {
+    borderVertical(x);
+    borderHorizontal(y);
+  }
 }
